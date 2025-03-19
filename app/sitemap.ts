a5677f02c3b8f6b29d19a89b113550673a139435
biswarup96 <biswarana96.rana@gmail.com>
2025-03-19 12:23:47 +0530

    SEO

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://portfolioo-five-mu.vercel.app/"; 
+  const pages = [
+    "#top", 
+    "#about",
+    "#work",
+    "#service",
+    "#contact",
+   
+  ];
+
+  return pages.map((page) => ({
+    url: `${baseUrl}`,
+    lastModified: new Date().toISOString(),
+    changeFrequency: "monthly",
+    priority: page === "" ? 1.0 : 0.8,
+  }));
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import { withSentryConfig } from "@sentry/nextjs";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  // Your existing Next.js configuration
 };
 
-export default nextConfig;
+// Make sure adding Sentry options is the last code to run before exporting
+module.exports = withSentryConfig(nextConfig, {
+  org: "https://portfolioo-five-mu.vercel.app/",
+  project: "Biswarup's Portofilo",
+
+  // Only print logs for uploading source maps in CI
+  // Set to `true` to suppress logs
+  silent: !process.env.CI,
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+});
